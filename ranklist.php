15c7513a6fd3d0b0ea3539a23c3cc2df7fd11aac
shitou <winipcss@outlook.com>
2015-05-13 11:23:44 +0800

    english and change path

--- ranklist.php
+++ ranklist.php
@@ -1,29 +1,29 @@
 <!DOCTYPE html>
 <html>
-    <head>???????????Campus campus Belle ranking selection?????
-        <title>Ca</title>
+    <head>
+        <title>Campus campus Belle ranking selection</title>
          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
         <meta http-equiv="X-UA-Compatibl\e" content="IE=edge">
-        <meta name="keywords" content="????????">
-        <meta name="description" content="????????,???????.">
-		<link href="http://libs.baidu.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
-    	<script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
+        <meta name="keywords" content="Campus campus Belle ranking selection">
+        <meta name="description" content="Campus campus Belle ranking selection,by shiyanlou">
+    <link href="bootstrap.css" rel="stylesheet">
+      <script src="jquery.js"></script>
     </head>
     <body>
     <div class="wrapper" id="wrapper">
         <nav class="navbar navbar-default navbar-static-top">
-              <a class="navbar-brand" href="index.php">??????(??? ????)</a>
+              <a class="navbar-brand" href="index.php">Campus Belle Rank (shiyanlou.com)</a>
         </nav>
         <!-- ^ ?? -->
         		<div class="container ranklist">
 			<table class="table">
-		      <caption>???????.</caption>
+		      <caption>Campus Belle Rank Table.</caption>
 		      <thead>
 		        <tr>
-		          <th>#</th>
-		          <th>??</th>
-		          <th>??</th>
+		          <th>Section</th>
+		          <th>Name</th>
+		          <th>RankScore</th>
 		        </tr>
 		      </thead>
 		      <tbody>
@@ -36,11 +36,11 @@
 		        <?php }?>
 		      </tbody>
 		    </table>
-        <button class="btn btn-success" id="rechoose">????</button>
+        <button class="btn btn-success" id="rechoose">Choose Again</button>
         </div>
         <!-- $ ?? -->
     </div>
-    <script src="http://libs.baidu.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <script src="bootstrap.js"></script>
     <script type="text/javascript">
     $("#rechoose").on("click",function(){
        document.cookie="rankwomanper=0";
